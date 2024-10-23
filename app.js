ce680220d1f4beefefd2c3b456028d95e24054e4
Anjara <sealthylight23@gmail.com>
2024-10-23 10:58:34 +0300

    Group Checkpoint

---
+++ app.js
@@ -0,0 +1,9 @@
+function display_menu(){
+    x=document.getElementById("menu-1");
+    if(x.style.display==="block"){
+        x.style.display = "none";
+    }
+    else{
+        x.style.display = "block";
+    }
+}
